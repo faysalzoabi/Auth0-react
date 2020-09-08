850839719c6a34c11bad2a83dd2bfa2d9d7b046f
faysal zoabi <zoabi@ecoinvent.org>
2020-09-08 22:14:03 +0200

    first commit

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
+
+function LogoutButton() {
+  const {logout, isAuthenticated} = useAuth0()
+  return (
+    isAuthenticated && (
+    <div>
+      <button onClick={() => logout()}>
+        Log Out
+      </button>
+    </div>
+    )
+  )
+}
+
+export default LogoutButton
